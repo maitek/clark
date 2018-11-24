4e38d180a17c9e1684eae3deeafbb7312cb5f2cb
Mathias Sundholm <mathias_sundholm@hotmail.com>
2018-11-24 23:34:32 +0100

    fixed add kernel

--- clark.py
+++ clark.py
@@ -4,23 +4,22 @@ import pyopencl as cl
 ctx = cl.create_some_context(interactive=False,answers=[0,1])
 queue = cl.CommandQueue(ctx)
 
-
 class Tensor():
     def __init__(self,data):
         self.data = data
+
         self.prg = cl.Program(ctx, """
             __kernel void add(
-                __global const float *a_g, __global const float *b_g, __global float *res_g)
+            __global const float *a_g, __global const float *b_g, __global float *res_g)
             {
-              int gid = get_global_id(0);
-              res_g[gid] = a_g[gid] + b_g[gid];
+              int x = get_global_id(0);
+              res_g[x] = a_g[x] + b_g[x];
             }
             """).build()
 
         @property
         def shape(self):
             return self.data.shape
-        
 
     def numpy(self):
         return self.data
@@ -38,13 +37,14 @@ class Tensor():
     def __add__(self,other):
 
         result = Tensor(np.empty_like(self.data))
-
+        
         mf = cl.mem_flags
         a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.data)
         b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=other.data)
 
         res_g = cl.Buffer(ctx, mf.WRITE_ONLY, self.data.nbytes)
-        self.prg.add(queue, self.data.shape, None, a_g, b_g, res_g)
+        
+        self.prg.add(queue, self.data.flatten().shape, None, a_g, b_g, res_g)
 
         cl.enqueue_copy(queue, result.data, res_g)
         return result
@@ -67,11 +67,11 @@ def randn(shape,dtype=np.float32):
 
 
 if __name__ == "__main__":
-    a = ones((50,10),np.float32)
-    b = ones((50,10),np.float32)
+    a = ones((45,20),np.float32)
+    b = ones((45,20),np.float32)
 
-    c = a + b
+    c = a + bb
     print(a.data, " + ", b.data, " = ", c.data)
 
-
+    
 
