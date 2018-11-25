1e043ee1b21ffdf4aec32b2c9f776d1a88a12ab2
Mathias Sundholm <mathias_sundholm@hotmail.com>
2018-11-25 01:03:39 +0100

    added matmult boilerplate code

--- clark.py
+++ clark.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pyopencl as cl
+from time import time
 
 ctx = cl.create_some_context(interactive=False,answers=[0,1])
 queue = cl.CommandQueue(ctx)
@@ -12,15 +13,35 @@ class Tensor():
             __kernel void add(
             __global const float *a_g, __global const float *b_g, __global float *res_g)
             {
-              int x = get_global_id(0);
-              res_g[x] = a_g[x] + b_g[x];
+              int row = get_global_id(0);
+              res_g[row] = a_g[row] + b_g[row];
             }
 
             __kernel void mul(
             __global const float *a_g, __global const float *b_g, __global float *res_g)
             {
-              int x = get_global_id(0);
-              res_g[x] = a_g[x] * b_g[x];
+              int row = get_global_id(0);
+              res_g[row] = a_g[row] * b_g[row];
+            }
+
+            // First naive implementation
+            __kernel void matmult(const int M, const int N, const int K,
+                                  const __global float* A,
+                                  const __global float* B,
+                                  __global float* C) {
+                
+                // Thread identifiers
+                const int row = get_global_id(0); // Row ID of C (0..M)
+                const int col = get_global_id(1); // Col ID of C (0..N)
+             
+                // Compute a single element (loop over K)
+                float acc = 0.0f;
+                for (int k=0; k<K; k++) {
+                    acc += A[k*M + row] * B[col*K + k];
+                }
+             
+                // Store the result
+                C[col*M + row] = acc;
             }
 
 
@@ -74,6 +95,22 @@ class Tensor():
         cl.enqueue_copy(queue, result.data, res_g)
         return result
 
+    def matmult(self,other):
+        # TODO fix this
+
+        result = Tensor(np.empty_like(self.data))
+        
+        mf = cl.mem_flags
+        a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.data)
+        b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=other.data)
+
+        res_g = cl.Buffer(ctx, mf.WRITE_ONLY, self.data.nbytes)
+        
+        self.prg.matmult(queue, self.data.flatten().shape, None, a_g, b_g, res_g)
+
+        cl.enqueue_copy(queue, result.data, res_g)
+        return result
+
 def from_numpy(ndarray):
     t = Tensor(ndarray.shape)
     t.data = ndarray
@@ -92,10 +129,23 @@ def randn(shape,dtype=np.float32):
 
 
 if __name__ == "__main__":
-    a = ones((45,20),np.float32)
-    b = ones((45,20),np.float32)
+    
+    a = np.ones((10000,10000),np.float32)
+    b = np.ones((10000,10000),np.float32)
 
+    tic = time()
     c = a + b
+    print(time()-tic)
+
+    a = ones((10000,10000),np.float32)
+    b = ones((10000,10000),np.float32)
+    
+    tic = time()
+    c = a + b
+    print(time()-tic)
+
+
+    
     print(a.data, " + ", b.data, " = ", c.data)
 
     
