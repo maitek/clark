150a62c30e72ae2074c86cc4aa30533d735221b9
Mathias Sundholm <mathias_sundholm@hotmail.com>
2019-06-20 15:23:41 +0200

    moved benchmark to own file

--- clark.py
+++ clark.py
@@ -139,34 +139,4 @@ def rand(shape,dtype=np.float32):
     return t
 
 
-if __name__ == "__main__":
 
-    size = 2000
-    num_runs = 5
-    print("Matrix size {} x {}:".format(size,size))
-    print("Number of runs {}".format(num_runs))
-
-    print("====== Clark matmult (gpu) ======")
-    
-    tic = time()
-    a = rand((size,size),np.float32).gpu()
-    b = rand((size,size),np.float32).gpu()
-    c = zeros((size,size),np.float32).gpu()
-    print("Time init: {}".format(time()-tic))
-
-    for i in range(num_runs):
-        c = a.matmult(b)
-    print("Time total: {}".format(time()-tic))
-    
-    
-    print("====== Numpy matmult (cpu) ======")
-    tic = time()
-    a = np.random.rand(size,size).astype(np.float32)
-    b = np.random.rand(size,size).astype(np.float32)
-    c = np.zeros((size,size),np.float32)
-    print("Time init: {}".format(time()-tic))
-
-    for i in range(num_runs):
-        c =np.matmul(a,b)
-    print("Time total {}".format(time()-tic))
-    
